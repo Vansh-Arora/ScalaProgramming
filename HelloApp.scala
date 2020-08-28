0103df907279e0ea5aa0bcc178926a878bdc6fcb
Vansh <vansharora2929@gmail.com>
2020-08-29 03:14:41 +0530

    Using command line arguments

--- HelloApp.scala
+++ HelloApp.scala
@@ -1,3 +1,8 @@
-object HelloApp extends App{
+object HelloApp extends App{  // App trait here has its own main method predefined
     println("Hello, World")
+    // Looking at automatically availabe command line arguments
+    if(args.length == 0)        // can use args.size as well
+        println("Hello, World!")
+    else
+        println("Hello,     " + args(0))
 }
