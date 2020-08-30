f39487d2417d40a262b4d6751af0bcedd1a92d6a
Vansh <vansharora2929@gmail.com>
2020-08-30 16:21:41 +0530

    Type is optional

--- Variables.scala
+++ Variables.scala
@@ -11,5 +11,17 @@ object Variables extends App{
     var y = 5
     // Reassigning to mutable variable.
     y = y + 1
+
+
+    // Type is optional
     
+    val count = 1
+    val num: Int = 1  // Use explicit form when u need to be clear or compiler may confuse it for other type
+
+    // For Example:
+    val i: Int = 1
+    val b: Byte = 1
+
+    val name = "Jack"
+    val c = 'V' 
 }
