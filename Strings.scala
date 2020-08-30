d8af8c448f57d3d39ede5f6b9aa094ddec1172bc
Vansh <vansharora2929@gmail.com>
2020-08-30 18:30:30 +0530

    String Interpolation:

---
+++ Strings.scala
@@ -0,0 +1,8 @@
+object Strings extends App{
+    val fname = "Van"
+    val mname = "C"
+    val lname = "Diego"
+
+    val fullname= s"$fname $mname $lname" // String Interpolation
+    println(fullname)
+}
