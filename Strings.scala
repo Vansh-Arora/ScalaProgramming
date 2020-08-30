4b973c16864112b72c386a50ecbade6d6e6038ea
Vansh <vansharora2929@gmail.com>
2020-08-30 18:41:10 +0530

    Multiline Strings

--- Strings.scala
+++ Strings.scala
@@ -5,4 +5,19 @@ object Strings extends App{
 
     val fullname= s"$fname $mname $lname" // String Interpolation
     println(fullname)
+
+
+// Output with indents
+    val speech = """ Hi, I am Vansh,
+                     I am practicing
+                     multiline strings
+                     in scala."""
+    println(speech)
+
+// Output without indents
+    val speech2 = """Hi, I am Vansh,
+                     |I am practicing
+                     |multiline strings
+                     |in scala.""".stripMargin
+    println(speech2)
 }
