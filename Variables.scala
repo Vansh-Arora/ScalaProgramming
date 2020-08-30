63d4e9d9e83bac9f60784b332b8d0387c8518fc9
Vansh <vansharora2929@gmail.com>
2020-08-30 16:06:33 +0530

    Types of Variables in Scala

---
+++ Variables.scala
@@ -0,0 +1,15 @@
+/* There are 2 types of variables in Scala:
+    Immutable: val
+    Mutable: var
+*/    
+object Variables extends App{
+
+    val x = 5;
+
+    //x = x + 1
+
+    var y = 5
+
+    y = y + 1
+    println(x,y)
+}
