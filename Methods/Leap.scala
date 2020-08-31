23a66793efc27e506cd4e240d5702d45ba4c4e2d
Vansh <vansharora2929@gmail.com>
2020-08-31 10:25:22 +0530

    Completed Leap year check

--- Methods/Leap.scala
+++ Methods/Leap.scala
@@ -1,19 +1,30 @@
+import scala.io.StdIn.readInt
 object Leap extends App{
-    def checkLeap(year: Int) = {
+    def checkLeap(year: Int): Unit = {
         if(year % 4 == 0)
         {
             if(year % 100 == 0)
             {
                 if(year % 400 == 0)
+                {
                     println("Leap year")
+                    return
+                }
                 else
+                {
                     println("Not a Leap Year")
+                    return
+                }
             }
+            
             println("Leap Year")
+            return
         }
         else
             println("Not a leap Year")
+            return
     }
 
-    checkLeap(2020)
+    var year = readInt(); 
+    checkLeap(year)
 }   
